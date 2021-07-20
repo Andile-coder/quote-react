644dadfdf5d1ae8b904702015766cae97fe3251c
Andile-coder <maselaandile@gmail.com>
2021-07-20 14:44:24 +0200

    random quote

--- src/quotes.js
+++ src/quotes.js
@@ -5,13 +5,17 @@ function Quote() {
   const [quote, setQuote] = useState([]);
   useEffect(() => {
     fetch("https://node-challenge-qoute-server.herokuapp.com/quote")
-      .then((response) => response.json)
+      .then((response) => response.json())
       .then((data) => setQuote(data));
   }, []);
 
   return (
     <div>
-      <p>helloworld {quote[1].quote} </p>
+      <p>
+        {quote.length > 0
+          ? quote[Math.floor(Math.random() * quote.length)].quote
+          : "waiting....."}
+      </p>
     </div>
   );
 }
